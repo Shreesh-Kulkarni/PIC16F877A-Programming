cfc1a736adb6e126133aa6eff64bea0d0299a044
Shreesh Kulkarni <kshreesh5@gmail.com>
2022-03-12 19:20:23 +0530

    Add files via upload

---
+++ Peripheral-Programs/LEDs/main.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.h
+ * Author: Shreesh Kulkarni
+ *
+ * Created on March 12, 2022, 3:54 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED_ARRAY PORTB
+#define LED_ARRAY_DDR TRISB
+
+#endif	/* MAIN_H */
+
